fee51d66743821125f880acb41b959c01b791fae
Ion19 <engabdelrhman2016@gmail.com>
2019-09-15 19:52:34 +0200

    Updates

---
+++ precache-manifest.63442061a184945ef5c6b3d5074f0b78.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfba54c0be877f7d1b94ce83016658f",
+    "url": "/wayfairtask/index.html"
+  },
+  {
+    "revision": "afabe0512b503093347c",
+    "url": "/wayfairtask/static/css/2.b81fa038.chunk.css"
+  },
+  {
+    "revision": "a80c8eb2a8f347bfa968",
+    "url": "/wayfairtask/static/css/main.631c5c22.chunk.css"
+  },
+  {
+    "revision": "afabe0512b503093347c",
+    "url": "/wayfairtask/static/js/2.c532cb1f.chunk.js"
+  },
+  {
+    "revision": "a80c8eb2a8f347bfa968",
+    "url": "/wayfairtask/static/js/main.2f414a94.chunk.js"
+  },
+  {
+    "revision": "714ca350303ae8b3e6e7",
+    "url": "/wayfairtask/static/js/runtime~main.0bc446a9.js"
+  },
+  {
+    "revision": "4f0022f25672c7f501c339cbf98d9117",
+    "url": "/wayfairtask/static/media/fontawesome-webfont.4f0022f2.ttf"
+  },
+  {
+    "revision": "776d58f453c8fe5d6a89e9c31ee223ff",
+    "url": "/wayfairtask/static/media/fontawesome-webfont.776d58f4.svg"
+  },
+  {
+    "revision": "90186830c9c50a0fed932494581761d9",
+    "url": "/wayfairtask/static/media/fontawesome-webfont.90186830.eot"
+  },
+  {
+    "revision": "fdf491ce5ff5b2da02708cd0e9864719",
+    "url": "/wayfairtask/static/media/fontawesome-webfont.fdf491ce.woff"
+  },
+  {
+    "revision": "1a5ff9bcdc25a4355b31ee147f652f7e",
+    "url": "/wayfairtask/static/media/header.1a5ff9bc.png"
+  },
+  {
+    "revision": "c9816181609f565be94843e1ef928361",
+    "url": "/wayfairtask/static/media/item-1.c9816181.png"
+  },
+  {
+    "revision": "9649cb1b682e2d13405b7f01858282c1",
+    "url": "/wayfairtask/static/media/item-2.9649cb1b.png"
+  },
+  {
+    "revision": "bbe9d0176b278d7a828b1e80043d352c",
+    "url": "/wayfairtask/static/media/item-3.bbe9d017.png"
+  },
+  {
+    "revision": "f7d7fff21ce959c63265d1c80627d99b",
+    "url": "/wayfairtask/static/media/item-4.f7d7fff2.png"
+  },
+  {
+    "revision": "17c6f5c12f2b74c1f5cc15830bb79184",
+    "url": "/wayfairtask/static/media/panoramaimage.17c6f5c1.png"
+  },
+  {
+    "revision": "7cd9d690ed274ac21a1155eb14698bbc",
+    "url": "/wayfairtask/static/media/product1.7cd9d690.png"
+  },
+  {
+    "revision": "037d34a8d620aa365910811ff4b260cf",
+    "url": "/wayfairtask/static/media/product2.037d34a8.png"
+  },
+  {
+    "revision": "69a11f41fbfd76abbb79e997d17038c6",
+    "url": "/wayfairtask/static/media/product3.69a11f41.png"
+  },
+  {
+    "revision": "00f66bbbb687d0fdb742661597298a3b",
+    "url": "/wayfairtask/static/media/product4.00f66bbb.png"
+  },
+  {
+    "revision": "0dd75327681ae12dddf3b3391e58b79b",
+    "url": "/wayfairtask/static/media/product5.0dd75327.png"
+  },
+  {
+    "revision": "8f69c361536bd432c05bdb24723621fd",
+    "url": "/wayfairtask/static/media/product6.8f69c361.png"
+  },
+  {
+    "revision": "a52fb0ee70eac0527e3593533314cf7a",
+    "url": "/wayfairtask/static/media/product7.a52fb0ee.png"
+  },
+  {
+    "revision": "7e9f452ccace235930f8f6515019085d",
+    "url": "/wayfairtask/static/media/product8.7e9f452c.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/wayfairtask/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/wayfairtask/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/wayfairtask/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/wayfairtask/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c231c170b46be4086fe78d4b621e9bd8",
+    "url": "/wayfairtask/static/media/slideritem1.c231c170.png"
+  },
+  {
+    "revision": "ac31938fc615aca4bb5d905a3aabfe3d",
+    "url": "/wayfairtask/static/media/slideritem3.ac31938f.png"
+  },
+  {
+    "revision": "e8bad2ac6512940398415630a56eb50b",
+    "url": "/wayfairtask/static/media/slidermain1.e8bad2ac.png"
+  },
+  {
+    "revision": "d4a5520e1bc62ac125be84be4b710315",
+    "url": "/wayfairtask/static/media/visa.d4a5520e.png"
+  }
+]);
